be025c0d1e150d38dc8fa489d8f3e11307247493
bfs213 <33587782+bfs213@users.noreply.github.com>
2020-10-30 09:43:54 +0800

    online 初始项目

---
+++ src/main/java/com/MyCourseOnline/MyCourseOnline/MyCourseOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.MyCourseOnline.MyCourseOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCourseOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyCourseOnline/MyCourseOnline/MyCourseOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyCourseOnline.MyCourseOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCourseOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
